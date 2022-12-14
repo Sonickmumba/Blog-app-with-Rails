45a5c1009f75189bdee383538a2feb282491fcf9
sonickmumba <kingellie.mumba@gmail.com>
2022-12-14 12:59:05 +0200

    Create model classes and associations between models

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'author_id'
+
+  # method to update the comments counter for a given post
+  def update_comment_counter_post
+    Post.increment!(:comments_counter)
+  end
+end
