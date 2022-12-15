9b1e3c180f00ad74d059a65e2e80a3f13e71668a
sonickmumba <kingellie.mumba@gmail.com>
2022-12-15 12:40:48 +0200

    Add post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
+  # post validations.
+  validates :title, presence: true, length: { maximum: 250 }, allow_blank: false
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0} 
+
   # menthod to update the post counter for a given user
   after_save :update_post_counter_user
 
