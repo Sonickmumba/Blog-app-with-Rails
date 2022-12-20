0e62fa867e440f0466f3a8c79b0d87129a598370
sonickmumba <kingellie.mumba@gmail.com>
2022-12-20 02:09:37 +0200

    Implement all test required for each controller advised by reviewer

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -6,12 +6,15 @@ RSpec.describe 'PostsController', type: :request do
       get '/users/:user_id/posts'
       expect(response).to have_http_status(:success)
       expect(response.body).to include('The page displays the posts for given user')
+      expect(response).to render_template('posts')
     end
   end
 
   describe 'posts#show' do
     it 'render the show template' do
       get '/users/2/posts/2'
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('This page display the posts by a user and the Comments for that post')
       expect(response).to render_template('show')
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe UsersController, type: :request do
     it 'returns http success' do
       get '/users/'
       expect(response).to have_http_status(:success)
+      expect(response).to render_template('users')
       expect(response.body).to include('The page displays the list of users for the Blog app')
     end
   end
@@ -12,7 +13,9 @@ RSpec.describe UsersController, type: :request do
   describe 'users#show' do
     it 'render the show template' do
       get '/users/:user_id/posts'
+      expect(response).to have_http_status(:success)
       expect(response).to render_template('posts')
+      expect(response.body).to include('The page displays the posts for given user')
     end
   end
 end
